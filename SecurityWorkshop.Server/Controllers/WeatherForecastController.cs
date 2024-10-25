903617db80952589b9805c878f1bc0e921d2419c
Balint Pogatsa <bp@macrix.pl>
2024-10-25 11:31:18 +0200

    added authorization and jwtbearer

--- SecurityWorkshop.Server/Controllers/WeatherForecastController.cs
+++ SecurityWorkshop.Server/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SecurityWorkshop.Server.Controllers
@@ -18,6 +19,7 @@ namespace SecurityWorkshop.Server.Controllers
       _logger = logger;
     }
 
+    [Authorize("read_access")]
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
--- SecurityWorkshop.Server/Program.cs
+++ SecurityWorkshop.Server/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 namespace SecurityWorkshop.Server
 {
   public class Program
@@ -13,6 +15,24 @@ namespace SecurityWorkshop.Server
       // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
       builder.Services.AddEndpointsApiExplorer();
       builder.Services.AddSwaggerGen();
+
+      builder.Services.AddAuthentication()
+        .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+        {
+          options.Authority = "https://localhost:8181/realms/quickstart";
+          options.Audience = "api://8814267c-25fc-459e-b0a6-f6d7ed056f12";
+          options.MapInboundClaims = false;
+        });
+
+      builder.Services.AddAuthorizationBuilder()
+       .AddPolicy("read_access", builder =>
+       {
+         builder.RequireClaim("scp", "workshop:read", "workshop:all");
+       })
+       .AddPolicy("write_access", builder =>
+       {
+         builder.RequireClaim("scp", "workshop:write", "workshop:all");
+       });
 
       var app = builder.Build();
 
