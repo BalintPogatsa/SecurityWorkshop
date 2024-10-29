1cc2a438dc676a9ef6a2bcdb6b0832eee9e3b3ab
Balint Pogatsa <bp@macrix.pl>
2024-10-29 12:57:54 +0100

    added missed classes

---
+++ SecurityWorkshop.Server/DataAccess/WeatherContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection.Metadata;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Options;
+
+namespace SecurityWorkshop.Server.DataAccess
+{
+
+  public class WeatherContext : DbContext
+  {
+    private readonly Action<WeatherContext, ModelBuilder> _modelCustomizer;
+
+    public string DbPath { get; }
+
+    #region Constructors
+    public WeatherContext()
+    {
+      var folder = Environment.SpecialFolder.LocalApplicationData;
+      var path = Environment.GetFolderPath(folder);
+      DbPath = System.IO.Path.Join(path, "blogging.db");
+    }
+
+    //public WeatherContext(DbContextOptions<WeatherContext> options, Action<WeatherContext, ModelBuilder> modelCustomizer = null)
+    //    : base(options)
+    //{
+    //  _modelCustomizer = modelCustomizer;
+    //}
+    #endregion
+
+    public DbSet<Blog> Blogs => Set<Blog>();
+
+    #region OnConfiguring
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlite($"Data Source={DbPath}");
+
+      //if (!optionsBuilder.IsConfigured)
+      //{
+      //  optionsBuilder.UseInMemoryDatabase("BloggingControllerTest")
+      //  .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+      //}
+    }
+    #endregion
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      
+      if (_modelCustomizer is not null)
+      {
+        _modelCustomizer(this, modelBuilder);
+      }
+    }
+  }
+}
